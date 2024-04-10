5e22efa95e55738b3966cb429c8396ca802f4e7d
victo123 <lifegs27@gmail.com>
2024-04-10 12:33:24 +0700

    test 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,30 @@ app.get('/', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.status(200).json({ msg: "route to /hello is ok" });
+})
+
+app.get('/test', (req, res) => {
+  https.get('https://jsonplaceholder.typicode.com/users', res => {
+    let data = [];
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.statusCode);
+    console.log('Date in Response header:', headerDate);
+  
+    res.on('data', chunk => {
+      data.push(chunk);
+    });
+  
+    res.on('end', () => {
+      console.log('Response ended: ');
+      const users = JSON.parse(Buffer.concat(data).toString());
+  
+      for(user of users) {
+        console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+      }
+    });
+  }).on('error', err => {
+    console.log('Error: ', err.message);
+  });
 })
 
 
