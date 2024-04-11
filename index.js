994a911cca65ccc48ef6ed323d7a599062d8742d
victo123 <lifegs27@gmail.com>
2024-04-11 07:02:38 +0700

    test2

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require("cors");
 const https = require('https');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }),
+       cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+       
+       );
 
 
 
