ef6780d3bdb42da796c504920d2c5a6000883b8c
Onkry Victor <lifegs27@gmail.com>
2024-07-19 16:12:16 +0700

    test5

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
 const express = require('express');
-/*
-const cors = require("cors");
-const https = require('https');
-*/
+const fetch = require('node-fetch');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json({ extended: false }),
-       cors({
-    origin: "*",
-    optionsSuccessStatus: 200,
-  })
-       
-       );
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/user');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-
-app.get('/', (req, res) => {
-  res.status(200).json({ msg: "Hello World" });
-})
-
-app.get('/hello', (req, res) => {
-  res.status(200).json({ msg: "route to /hello is ok" });
-})
-
-/*
-app.get('/test', (req, res) => {
-  https.get('https://jsonplaceholder.typicode.com/users', res => {
-    let data = [];
-    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-    console.log('Status Code:', res.statusCode);
-    console.log('Date in Response header:', headerDate);
-  
-    res.on('data', chunk => {
-      data.push(chunk);
-    });
-  
-    res.on('end', () => {
-      console.log('Response ended: ');
-      const users = JSON.parse(Buffer.concat(data).toString());
-  
-      for(user of users) {
-        console.log(`Got user with id: ${user.id}, name: ${user.name}`);
-      }
-    });
-  }).on('error', err => {
-    console.log('Error: ', err.message);
-  });
-})
-*/
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => console.log(`Server is running in PORT ${PORT}`));
 
