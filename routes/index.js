a5ceb6c3916a5d2c72e25ac892349844f6b474bb
Onkry Victor <lifegs27@gmail.com>
2024-07-19 23:03:32 +0700

    add fare

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,17 @@ router.get('/', function(req, res, next) {
 
 router.get('/hello', function(req, res, next) {
     res.status(200).json({ msg: "route to /hello is ok" });
+});
+
+router.get('/fare', function(req, res, next) {
+    fetch('https://next-api.airpaz.com/v1/fl/flight/fare-calendar?depAirport=JKTA&arrAirport=SUB&currency=IDR&isRoundTrip=false&tripType=economy')
+    .then((res) => res.json())
+    .then((data) => {
+        res.send({ data });
+    })
+    .catch((err) => {
+        res.send(err);
+    });
 });
 
 module.exports = router;
